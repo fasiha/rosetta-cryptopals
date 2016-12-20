4e1316ad70d228031c848fcc440c79ccab338e1c
Ahmed Fasih <wuzzyview@gmail.com>
2016-12-20 13:38:05 -0500

    For loop to copy subvecs

--- cryptobasics/src/main.rs
+++ cryptobasics/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     cryptobasics::hex2bytes::demo();
     cryptobasics::base64encode::demo();
 
+    // Extra testing code.
     let s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let bytes = cryptobasics::hex2bytes::hex2bytes(s);
     println!("Got: {}", str::from_utf8(&bytes).unwrap());
